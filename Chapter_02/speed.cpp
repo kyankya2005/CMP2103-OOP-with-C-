93513f56908686fe203b0fa8f59f88bc85d41dce
Kyankya Joseph <jkyankya10@gmail.com>
2026-09-09 18:28:40 +0300

    Completed assignment for calculating average speed

--- Chapter_02/speed.cpp
+++ Chapter_02/speed.cpp
@@ -2,3 +2,26 @@
 // Write a program that displays the average speed in miles per hour. (Note that
 // 1 mile is 1.6 kilometers.)
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double kilometers = 14;
+
+    // Time: 45 minutes and 30 seconds
+    double minutes = 45;
+    double seconds = 30;
+
+    // Converting kilometers to miles
+    double miles = kilometers / 1.6;
+
+    // Converting time to hours
+    double hours = (minutes * 60 + seconds) / 3600;
+
+    double speed = miles / hours;
+
+    cout << "The average speed is " << speed << " miles per hour." << endl;
+
+    return 0;
+}
