f548e504df48de8ad564ae4840060497481274ce
Kyankya Joseph <jkyankya10@gmail.com>
2026-09-09 19:22:56 +0300

    powerlossAssignment

--- Chapter_02/power_loss.cpp
+++ Chapter_02/power_loss.cpp
@@ -4,3 +4,28 @@
 // Write a program that prompts the user to enter L, A, resistivity, and current,
 // and returns the power loss of a transmission line.
 
+#include <iostream>
+
+int main() {
+    double L, A, resistivity, current;
+
+    std::cout << "Enter length L (m): ";
+    std::cin >> L;
+
+    std::cout << "Enter cross-sectional area A (m^2): ";
+    std::cin >> A;
+
+    std::cout << "Enter resistivity (ohm*m): ";
+    std::cin >> resistivity;
+
+    std::cout << "Enter current (A): ";
+    std::cin >> current;
+
+    double resistance = resistivity * (L / A);
+    double powerLoss = resistance * current * current;
+
+    std::cout << "\nResistance = " << resistance << " ohms\n";
+    std::cout << "Power loss = " << powerLoss << " watts\n";
+
+    return 0;
+}
